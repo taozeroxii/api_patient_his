610fc49367dfefa205cfe9e59acb9898be891013
unknown <tao32040@gmail.com>
2021-03-08 07:22:43 +0700

    เพิ่ม package cors
    ปรับแก้ส่วน cors origin

--- routes/patientinfomations.js
+++ routes/patientinfomations.js
@@ -2,10 +2,72 @@ const router = require('express').Router();
 const service = require('../service/patientService')
 const {check,validationResult} = require('express-validator')
 
+const cors = require('cors')
+router.use(cors());
+let corsOptions = {
+    origin:true,
+    methods:['GET','POST','OPTIONS'],
+    allowHeaders:['Content-Type','Authorization']
+}
+const   passport = require('passport');
+
 router.get('/',async(req,res)=>{
   //  res.json(await service.find());
   res.send('respond with a PatientInfomations');
 });
+
+
+router.get('/getdatabyhn/:hn',passport.authenticate('jwt', {session: false}), cors(corsOptions),
+async (req,res)=>  {
+    console.log(req.params.hn); // get method ดึงค่าจาก url :id
+    //console.log(req.body); //  ดึงค่าจากฟอร์มและส่งแบบ post
+    try{
+        // validationResult(req).throw();
+        //req.validate();
+        const model = await service.findOnegett(req.params.hn);
+        res.status(200).json(model);
+    }catch(ex){
+        // res.status(400).json({message:ex.message}) ///การเรียกใช้เดิมไม่สามารถรู้ได้ error ตัวไหน
+        res.error(ex)// เรียกฝช้จาก middleware ที่ถูกใช้งานผ่าน index file
+    }
+ 
+});
+
+
+router.get('/getdatabyvn/:vn', passport.authenticate('jwt', {session: false}),cors(corsOptions),
+async (req,res)=>  {
+    //console.log(req.params.hn); // get method ดึงค่าจาก url :id
+    //console.log(req.body); //  ดึงค่าจากฟอร์มและส่งแบบ post
+    try{
+        // validationResult(req).throw();
+        //req.validate();
+        const model = await service.findOnegetvn(req.params.vn);
+        res.status(200).json(model);
+    }catch(ex){
+        // res.status(400).json({message:ex.message}) ///การเรียกใช้เดิมไม่สามารถรู้ได้ error ตัวไหน
+        res.error(ex)// เรียกฝช้จาก middleware ที่ถูกใช้งานผ่าน index file
+    }
+ 
+});
+
+
+router.get('/getdatabyan/:an', passport.authenticate('jwt', {session: false}),cors(corsOptions),
+async (req,res)=>  {
+    //console.log(req.params.hn); // get method ดึงค่าจาก url :id
+    //console.log(req.body); //  ดึงค่าจากฟอร์มและส่งแบบ post
+    try{
+        // validationResult(req).throw();
+        //req.validate();
+        const model = await service.findOnegetan(req.params.an);
+        res.status(200).json(model);
+    }catch(ex){
+        // res.status(400).json({message:ex.message}) ///การเรียกใช้เดิมไม่สามารถรู้ได้ error ตัวไหน
+        res.error(ex)// เรียกฝช้จาก middleware ที่ถูกใช้งานผ่าน index file
+    }
+ 
+});
+
+
 
 router.post('/getdatabyhn',
 [check('hn','ไม่ได้ใส่ข้อมูล  หรือ กรอกไม่ครบ 9 หลัก').not().isEmpty().isLength({ min: 9 })],
@@ -23,6 +85,39 @@ async (req,res)=>  {
     }
  
 });
+
+router.post('/getdatabyvn',
+[check('vn','ไม่ได้ใส่ข้อมูล  หรือ กรอกไม่ครบ 12 หลัก').not().isEmpty().isLength({ min: 12 })],
+async (req,res)=>  {
+    try{
+        // validationResult(req).throw();
+        req.validate();
+        const model = await service.findOnevn( req.body);
+        res.status(200).json(model);
+    }catch(ex){
+        // res.status(400).json({message:ex.message}) ///การเรียกใช้เดิมไม่สามารถรู้ได้ error ตัวไหน
+        res.error(ex)// เรียกฝช้จาก middleware ที่ถูกใช้งานผ่าน index file
+    }
+ 
+});
+
+
+router.post('/getdatabyan',
+[check('an','ไม่ได้ใส่ข้อมูล  หรือ กรอกไม่ครบ 9 หลัก').not().isEmpty().isLength({ min: 9 })],
+async (req,res)=>  {
+    try{
+        req.validate();
+        const model = await service.findOnean( req.body);
+        res.status(200).json(model);
+    }catch(ex){
+        // res.status(400).json({message:ex.message}) ///การเรียกใช้เดิมไม่สามารถรู้ได้ error ตัวไหน
+        res.error(ex)// เรียกฝช้จาก middleware ที่ถูกใช้งานผ่าน index file
+    }
+ 
+});
+
+
+
 
 
 module.exports = router;
--- service/patientService.js
+++ service/patientService.js
@@ -23,14 +23,70 @@ module.exports = {
         });
     },
 
+    findOnegett(value) {
+        console.log(value);
+        return new Promise ((resolve,reject)=>{
+            pgconnection
+            .query(`SELECT pname,fname,lname,sex as gender,
+                case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text
+               -- ,cz.emp_citizenship_name as citizen,pt.addrpart,pt.moopart,pt.tmbpart,pt.amppart,pt.chwpart,concat(pt.addrpart,' หมู่ ',pt.moopart,ta.full_name) as fulladdress,
+               -- pt.po_code as post_code,   mobile_phone_number ,death ,fathername,pt.motherlname,firstday as firstvisit,marrystatus,mt.name as marryname,type_area
+                FROM patient pt
+                LEFT JOIN emp_citizenship cz on cz.emp_citizenship_id  = pt.citizenship
+                LEFT JOIN marrystatus mt on mt.code = pt.marrystatus
+                LEFT JOIN pttype pty on pty.pttype = pt.pttype
+                LEFT JOIN thaiaddress ta on ta.addressid = concat(pt.chwpart,pt.amppart,pt.tmbpart)  where hn=$1::text`,[value],(error,result)=>{
+                if(error) return reject(error)
+               // resolve(result.length > 0 ? result[0] : null)
+               resolve(result.rows[0])
+            } )
+        })
+    },
+
+    findOnegetvn(value) {
+        console.log(value);
+        return new Promise ((resolve,reject)=>{
+            pgconnection
+            .query(`SELECT pname,fname,lname,sex as gender,
+            case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text,
+            ov.pt_priority,pp.name as pt_priority_name
+            FROM ovst  ov
+            INNER JOIN patient pt on pt.hn = ov.hn
+            left JOIN pt_priority pp on pp.id = ov.pt_priority
+            where vn = $1::text`,[value],(error,result)=>{
+                if(error) return reject(error)
+               // resolve(result.length > 0 ? result[0] : null)
+               resolve(result.rows[0])
+            } )
+        })
+    },
+
+    findOnegetan(value) {
+        console.log(value);
+        return new Promise ((resolve,reject)=>{
+            pgconnection
+            .query(`SELECT pname,fname,lname,sex as gender,
+            case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text,
+            ov.pt_priority,pp.name as pt_priority_name
+            FROM ovst  ov
+            INNER JOIN patient pt on pt.hn = ov.hn 
+            left JOIN pt_priority pp on pp.id = ov.pt_priority
+            where an=$1::text`,[value],(error,result)=>{
+                if(error) return reject(error)
+               // resolve(result.length > 0 ? result[0] : null)
+               resolve(result.rows[0])
+            } )
+        })
+    },
+
     findOne(value) {
         //console.log(value.hn);
         return new Promise ((resolve,reject)=>{
             pgconnection
-            .query(`SELECT hn,cid as personal_id,pt.pttype,pty.name as pttypename,pname,fname,lname,sex as gender,
+            .query(`SELECT pname,fname,lname,sex as gender,
                 case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text
-                ,cz.emp_citizenship_name as citizen,pt.addrpart,pt.moopart,pt.tmbpart,pt.amppart,pt.chwpart,concat(pt.addrpart,' หมู่ ',pt.moopart,ta.full_name) as fulladdress,
-                pt.po_code as post_code,   mobile_phone_number ,death ,fathername,pt.motherlname,firstday as firstvisit,marrystatus,mt.name as marryname,type_area
+               -- ,cz.emp_citizenship_name as citizen,pt.addrpart,pt.moopart,pt.tmbpart,pt.amppart,pt.chwpart,concat(pt.addrpart,' หมู่ ',pt.moopart,ta.full_name) as fulladdress,
+               -- pt.po_code as post_code,   mobile_phone_number ,death ,fathername,pt.motherlname,firstday as firstvisit,marrystatus,mt.name as marryname,type_area
                 FROM patient pt
                 LEFT JOIN emp_citizenship cz on cz.emp_citizenship_id  = pt.citizenship
                 LEFT JOIN marrystatus mt on mt.code = pt.marrystatus
@@ -41,6 +97,43 @@ module.exports = {
                resolve(result.rows[0])
             } )
         })
+    },
+
+
+    findOnevn(value) {
+        return new Promise ((resolve,reject)=>{
+            pgconnection
+            .query(`SELECT pname,fname,lname,sex as gender,
+                case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text,
+                ov.pt_priority,pp.name as pt_priority_name
+                FROM ovst  ov
+                INNER JOIN patient pt on pt.hn = ov.hn
+                left JOIN pt_priority pp on pp.id = ov.pt_priority
+                where vn = $1::text`,[value.vn],(error,result)=>{
+                if(error) return reject(error)
+               // resolve(result.length > 0 ? result[0] : null)
+               resolve(result.rows[0])
+            } )
+        })
+    },
+
+    findOnean(value) {
+        return new Promise ((resolve,reject)=>{
+            pgconnection
+            .query(`SELECT pname,fname,lname,sex as gender,
+                case when sex = '1' then 'ชาย' else 'หญิง' END as gender_text,
+                ov.pt_priority,pp.name as pt_priority_name
+                FROM ovst  ov
+                INNER JOIN patient pt on pt.hn = ov.hn 
+                left JOIN pt_priority pp on pp.id = ov.pt_priority
+                where an=$1::text`,[value.an],(error,result)=>{
+                if(error) return reject(error)
+               // resolve(result.length > 0 ? result[0] : null)
+               resolve(result.rows[0])
+            } )
+        })
     }
+
+
 
 }
